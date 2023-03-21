dce2bef72f7c5697b3f8a0992ebfee787712c3a0
gapavoyan <gap_avoyan@mail.ru>
2023-03-21 21:14:25 +0400

    create Header

---
+++ src/header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./header.css"
+function Header() {
+    return (
+        <div>
+            <header>
+                <div className='imgDiv'>
+                    <img src='https://aitschool.am/images/Ait-Logo.svg' />
+                </div>
+                <div className='navDiv'>
+                    <nav>
+                        <ul className='navList'>
+                            <li>Գլխավոր</li>
+                            <li>Դասընթացներ</li>
+                            <li>Գրանցում</li>
+                            <li>Մեր մասին</li>
+                            <li>Բլոգ</li>
+                            <li>Կապ</li>
+                        </ul>
+
+                    </nav>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Header
